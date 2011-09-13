9dc8308d0a0f0beb047f68e2b0fa32cf71a3c73c
Scott M Parrish <anithri@gmail.com>
2011-09-12 18:33:57 -0600

    refactored specs to put all specs in file under a single describe block

--- spec/region_spec.rb
+++ spec/region_spec.rb
@@ -1,32 +1,30 @@
 require 'spec_helper'
+describe Gotham::Region do
+  describe "#new" do
+    it "should initialize with just name parameter" do
+      region = Gotham::Region.new("testing")
 
-describe Gotham::Region, "#new" do
-  it "should initialize with just name parameter" do
-    region = Gotham::Region.new("testing")
+      region.name.should == "testing"
+    end
 
-    region.name.should == "testing"
-    region.difficulty.should == 0
+    it "should always have a block hash" do
+      region = sample_region
 
-  end
+      region.blocks.should be_a(Hash)
+      region.blocks.should have(10).blocks
+    end
 
-  it "should always have a block hash" do
-    region = sample_region
+    it "should add a random path between 0 and another block" do
+      region  = sample_region
+      path_to = region.blocks[0].paths[0]
 
-    region.blocks.should be_a(Hash)
-    region.blocks.should have(10).blocks
-  end
-
-  it "should add a random path between 0 and another block" do
-    region = sample_region
-    path_to = region.blocks[0].paths[0]
-
-    path_to.should be_between(0,9)
-    region.blocks.should have_key(0)
-    region.blocks[0].should have(1).paths
-    region.blocks[path_to].paths.should include(0)
+      path_to.should be_between(0, 9)
+      region.blocks.should have_key(0)
+      region.blocks[0].should have(1).paths
+      region.blocks[path_to].paths.should include(0)
+    end
   end
 end
-
 private
 
 def sample_region
